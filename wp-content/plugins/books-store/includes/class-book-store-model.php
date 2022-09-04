506d4c4879bd08454f207c68f200f9c314478294
kshah3318 <kshah3318@gmail.com>
2022-09-04 11:16:53 +0530

    Added code for numberic pagination

--- wp-content/plugins/books-store/includes/class-book-store-model.php
+++ wp-content/plugins/books-store/includes/class-book-store-model.php
@@ -77,10 +77,13 @@ class Book_Store_Model {
 			'hide_empty' => true,
 		) );
 
+		$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+		
 		$get_data_args = array(
 			'post_type'      => 'book_store_book',
-			'posts_per_page' => -1,
+			'posts_per_page' => 5,
 			'post_status'    => 'publish',
+			'paged' => $paged,
 			'order' => 'ASC',
 		);
 		$books_data = new WP_Query($get_data_args);
@@ -204,6 +207,25 @@ class Book_Store_Model {
 				endif; 
 			?>
 			</table>
+			<nav class="pagination">
+            <div class="pager">
+                <?php
+                               $total_pages = $books_data->max_num_pages;
+							   $big = 999999999; // need an unlikely integer
+						   
+							   if ($total_pages > 1){
+								   $current_page = max(1, get_query_var('page'));
+						   
+								   echo paginate_links(array(
+									   'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+									   'format' => '?page=%#%',
+									   'current' => $current_page,
+									   'total' => $total_pages,
+								   ));
+							   }
+                ?>
+            </div>
+        </nav>
 		<?php
 	
 		// return the buffer contents and delete
